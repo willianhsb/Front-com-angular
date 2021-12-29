21659cae02c926f8f1a0c85086e0718e51970b24
willianhsb <68523991+willianhsb@users.noreply.github.com>
2021-12-28 22:45:28 -0300

    Add files via upload

---
+++ cadastro-create/cadastro-create.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { CadastroService } from '../cadastro.service';
+import { Router } from '@angular/router';
+import { Cadastro } from '../cadastro.model';
+
+@Component({
+  selector: 'app-cadastro-create',
+  templateUrl: './cadastro-create.component.html',
+  styleUrls: ['./cadastro-create.component.css']
+})
+export class CadastroCreateComponent implements OnInit {
+
+  cadastro: Cadastro = {
+    email:'',
+    cpf:'',
+    nome:'',
+    dataNasc:'',
+    telefone:'',
+    senha:''
+  }
+
+   constructor(private cadastroService: CadastroService, private router: Router ) { }
+
+  ngOnInit(): void {
+  }
+
+  createCadastro(): void{
+    this.cadastroService.create(this.cadastro).subscribe(() => {
+      this.cadastroService.showMessege('Usuário Cadastrado!')
+      this.router.navigate(['/cadastro/tabela'])
+    })
+  }
+
+  cancelarCadastro(): void{
+    this.router.navigate([''])
+  } 
+
+  tabelasCadastro(): void{
+    this.router.navigate(['/cadastro/tabela'])
+  }
+
+ 
+
+}
---
+++ cadastro.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Cadastro } from './cadastro.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+
+  baseUrl = "http://localhost:3000/login"
+
+  constructor(private snackBar: MatSnackBar, private http:) {
+    this.snackbar.open(msg, 'x', {
+      duration: 6000, 
+      verticalPosition: "top"
+    })
+   }
+
+   create(cadastro: Cadastro): Observable<Cadastro>{
+     return this.http.post<Cadastro>(this.baseUrl, cadastro)
+   }
+
+   read(): Observable<Cadastro[]>{
+     return this.http.get<Cadastro[]>(this.baseUrl)
+   }
+
+   readById(id: number): Observable<Cadastro> {
+     const url = `${this.baseUrl}/${id}`
+     return this.http.get<Cadastro>(url)
+   }
+
+   updateCadastro(cadastro: Cadastro): Observable<Cadastro>{
+    const url = `${this.baseUrl}/${cadastro.id}`
+    return this.http.put<Cadastro>(url, cadastro)
+   }
+
+   deleteCadastro(id: number): Observable<Cadastro>{
+    const url = `${this.baseUrl}/${id}`
+     return this.http.delete<Cadastro>(url)
+   }
+    
+}
