a45ea90e4b817ef3146c2c9bf6d82c2c6505b084
willianhsb <68523991+willianhsb@users.noreply.github.com>
2021-12-29 00:06:33 -0300

    Add files via upload

---
+++ cadastro-update/cadastro-update.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { CadastroService } from '../cadastro.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Cadastro } from '../cadastro.model';
+
+@Component({
+  selector: 'app-cadastro-update',
+  templateUrl: './cadastro-update.component.html',
+  styleUrls: ['./cadastro-update.component.css']
+})
+export class CadastroUpdateComponent implements OnInit {
+
+  cadastro: Cadastro;
+
+  constructor(private cadastroService: CadastroService, 
+              private router: Router, 
+              private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.cadastroService.readById(id).subscribe(cadastro => {
+      this.cadastro = cadastro
+    });
+  }
+
+  updateCadastro(): void{
+    this.cadastroService.updateCadastro(this.cadastro).subscribe(() => {
+      this.cadastroService.showMessege('O usuário foi atualizado')
+      this.router.navigate(["/cadastro/tabela"]);
+    });
+  }
+
+  cancelarCadastro(): void{
+    this.router.navigate(['/cadastro'])
+
+  }
+  
+  tabelasCadastro(): void{
+    this.router.navigate(['/cadastro/tabela'])
+
+  }
+
+}
